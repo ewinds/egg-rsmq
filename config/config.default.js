42b03243500d161cc7731c7172e2abc5cbbb530e
ewinds <dutboxer@me.com>
2019-09-11 13:53:58 +0800

    fix: 不依赖 egg-redis，有连接问题

--- config/config.default.js
+++ config/config.default.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
 /**
  * egg-mysql-paginator default config
  * @member Config#rsmq
  * @property {String} SOME_KEY - some description
  */
-exports.rsmq = {};
+exports.rsmq = { host: '127.0.0.1', port: 6379 };
